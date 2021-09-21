efda13a47d75e1db14cf2b3135ce1b2ee2adb6f9
zsline <sergei.dereka@gmal.com>
2021-09-21 12:23:28 +0300

    added slider 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-
+  $('.read__slider').slick({
+    dots: false,
+    prevArrow: "<img class='a-left control-c prev slick-prev' src='../images/arrow-prev.svg'>",
+    nextArrow: "<img class='a-right control-c next slick-next' src='../images/arrow-next.svg'>",
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+  });
+  
 })
